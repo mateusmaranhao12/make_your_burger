47e81af79cc7ec34e86d1a9d3df25e8acbe1dc1b
Mateus Maranhão <feradourada@gmail.com>
2023-02-10 19:35:25 -0300

    Create Banner.vue

---
+++ src/components/Banner.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="main-banner">
+      <h1>Make Your Burger</h1>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: "Banner"
+}
+</script>
+
+<style lang="scss">
+    @import '@/scss/index';
+</style>
