b3a734b8f2f4693320e134d02de298359f2f1854
Mateus Maranhão <feradourada@gmail.com>
2023-02-10 19:31:21 -0300

    Implements Banner.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,20 @@
 <template>
-  <p>Banner</p>
+  <Banner />
   <div class="main-container">
     <h1>Monte seu Burger</h1>
+    <p>formulario</p>
   </div>
 </template>
 
 <script>
+
+  import Banner from '@/components/Banner.vue'
+
+  export default {
+    name: 'Home',
+    components: {
+      Banner
+    }
+  }
+
 </script>
